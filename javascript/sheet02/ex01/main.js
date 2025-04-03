b75bebd0007dce1945ae1c0390e3b8e6b94b703d
Dorianaadc <doriana.ja2025@gmail.com>
2025-04-03 12:41:44 +0200

    sheet 02

---
+++ javascript/sheet02/ex01/main.js
@@ -0,0 +1,7 @@
+/*EX01: Filtrar números impares
+Escribe una función filtrarImpares que le pases un array y te devuelva un
+nuevo array con los números impares. Pista: usa filter .
+// Ejemplo de uso
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(filtrarImpares(numeros)); // [1, 3, 5, 7, 9]
+*/
