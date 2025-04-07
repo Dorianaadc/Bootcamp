acb4363ff85471b1c71ed646c1e9aeef1d7312ca
Dorianaadc <doriana.ja2025@gmail.com>
2025-04-07 12:15:55 +0200

    lista sheet03 js

---
+++ javascript/sheet03/ex03/main.js
@@ -0,0 +1,9 @@
+/*ex03. Función de flecha sin parámetros */
+
+// Función normal
+const saludar = function() {
+    return "Hola";
+    };
+    
+// Función de flecha
+const saludar2 = () => "Hola";
---
+++ javascript/sheet03/ex04/main.js
@@ -0,0 +1,11 @@
+/* 4. Función con varias líneas de código*/
+
+// Función normal
+const multiplica = function(a, b) {
+    const resultado = a * b;
+    return resultado;
+};
+
+
+// Función de flecha
+const multiplica2 = (a, b) => a * b;
