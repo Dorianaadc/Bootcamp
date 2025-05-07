c12509c82e56f405233b476ebf6c0b6d8aabb6ff
Dorianaadc <doriana.ja2025@gmail.com>
2025-05-07 11:49:29 +0200

    ejercicios

--- java/sheet01/ex08/Palindromo.java
+++ java/sheet01/ex08/Palindromo.java
@@ -11,6 +11,24 @@ Scanner sc = new Scanner(System.in);
 System.out.println("Escriba una cadena");
 String cadena = sc.nextLine();
 
+char[] caracteres = new char[cadena.length()];
 
+for (int i = 0; i < cadena.length(); i++) {
+    caracteres[i] = cadena.charAt(i);
+}
+
+boolean esPalindromo = true;
+for (int i = 0; i < cadena.length() / 2; i++) {
+if(caracteres[i] != caracteres[cadena.length() - 1 - i]){
+esPalindromo = false;
+    break;
+}
     }
+
+    if(esPalindromo){
+        System.out.println("Es palindromo");
+    }else{
+        System.out.println("No es palindromo");
+    }
+  }
 }
